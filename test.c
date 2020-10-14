b87dbca28968f502a54f18ade56731bb89cc1043
vishal464 <rajvishal947269@gmail.com>
2020-10-14 13:29:55 +0530

    trial 2

--- test.c
+++ test.c
@@ -2,5 +2,17 @@
 int main()
 {
     printf("Hello World");
+    int size;
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", i * 5);
+        if (i == 9)
+        {
+            printf("Hey");
+        }
+    }
+    if (true)
+        printf("Hello");
     return 0;
 }
